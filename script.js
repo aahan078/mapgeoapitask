3d9db8120eb8f246bc2ba2b4f3e79ff78e147bce
aahan078 <aahan.solanki@vkaps.com>
2023-04-11 14:01:08 +0530

    adding new alert

--- script.js
+++ script.js
@@ -10,15 +10,19 @@ let longitude = position.coords.longitude;
 
 const main =document.getElementById("location");
 
-const box= document.createElement("p");
-box.className="latitude"
-box.innerHTML= `Your latitude is - ${latitude}` 
+// const box= document.createElement("p");
+// box.className="latitude"
+// box.innerHTML= `Your latitude is - ${latitude}` 
 
-const box2 = document.createElement("p");
-box2.className="longitude"
-box2.innerHTML= `Your longitude is - ${longitude}` ;
+// const box2 = document.createElement("p");
+// box2.className="longitude"
+// box2.innerHTML= `Your longitude is - ${longitude}` ;
 
-main.append(box,box2);
+// main.append(box,box2);
+
+alert(`Your Latitide is - ${latitude} 
+Your Longitude is -${longitude}`)
+
 
 
 
